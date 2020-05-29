c6010a7a66007babfffbc04b2ac5a162907ba72f
Julia Seidman <juliaseid@gmail.com>
2020-05-29 09:49:38 -0700

    Create stylist model and corresponding data table

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public Category()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Days { get; set; }
+    public bool Monday { get; set; }
+    public bool Tuesday { get; set; }
+    public bool Wednesday { get; set; }
+    public bool Thursday { get; set; }
+    public bool Friday { get; set; }
+    public bool Saturday { get; set; }
+    public bool Sunday { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+}
